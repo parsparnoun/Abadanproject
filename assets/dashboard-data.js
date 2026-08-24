d9ca31a0931041d16ade2a763b2e768df5919e16
parsparnoun <parsparnoun1383@gmail.com>
2026-08-24 12:30:14 +0330

    Add files via upload

---
+++ assets/dashboard-data.js
@@ -0,0 +1,97 @@
+window.DASHBOARD_DATA = {
+  "settings": { 
+    "dashboard_title": "موضوع: خرید خدمات مشاوره طراحی معماری پروژه فروشگاه شهروند ماهان کرج",
+    "organization_name": "مهندسان مشاور پارس پرنون",
+    "updated_at": "شهریورماه ۱۴۰۵"
+  },
+  "pages": [
+    {
+      "page_id": "design",
+      "title": "طراحی",
+      "subtitle": "مدیریت اسناد و نقشه‌های طراحی",
+      "icon": "design",
+      "order": 1,
+      "sections": [
+        {
+          "section_id": "s1",
+          "title": "1- شرح خدمات مطالعات",
+          "attachments": [
+            { "title": "شرح خدمات قرارداد طراحی", "file_name": "https://drive.google.com/file/d/1YLr-sjkWz-akPn_m4jVVHVkE3lFRUQFP/view?usp=sharing" }
+          ]
+        },
+        {
+          "section_id": "s2",
+          "title": "2- مطالعات مرحله اول",
+          "attachments": [
+            { "title": "معماری", "description": "نقشه‌های مقدماتی و رندرهای اولیه سه بعدی", "file_name": "assets/Architecture_Pre.rar" },
+            { "title": "سازه", "description": "نقشه‌های مقدماتی سازه", "file_name": "assets/Structure_Pre.rar" },
+            { "title": "تاسیسات مکانیکی", "description": "نقشه‌های مقدماتی تاسیسات مکانیکی", "file_name": "assets/M&E_Pre.rar" },
+            { "title": "تاسیسات برقی", "description": "نقشه‌های مقدماتی تاسیسات برقی", "file_name": "assets/M&E_Pre.rar" }
+          ]
+        },
+        {
+          "section_id": "s3",
+          "title": "3- مطالعات مرحله دوم",
+          "attachments": [
+            { 
+              "title": "معماری", 
+              "description": "نقشه‌های نهایی معماری و رندرهای نهایی سه بعدی", 
+              "file_name": "https://drive.google.com/file/d/1eWMd3AEyOe778vBYQMJbZvSt2zrsD9BE/view?usp=sharing" 
+            },
+            { 
+              "title": "سازه", 
+              "description": "نقشه‌های نهایی سازه، دفترچه محاسبات و آزمایش ژئوتکنیک", 
+              "file_name": "https://drive.google.com/file/d/1BnmV4CfFkiBdEIZQPNJLEFrm5yxO-soG/view?usp=sharing" 
+            },
+            { 
+              "title": "تاسیسات مکانیکی", 
+              "description": "نقشه‌های نهایی تاسیسات مکانیکی، دفترچه محاسبات", 
+              "file_name": "https://drive.google.com/file/d/13cFY2tuLH-qqCec2im0Bp3sSZPHuuS1f/view?usp=sharing" 
+            },
+            { 
+              "title": "تاسیسات برقی", 
+              "description": "نقشه‌های نهایی تاسیسات برقی، دفترچه محاسبات", 
+              "file_name": "https://drive.google.com/file/d/1G5gpCmzKFU03XCKUJ6d3b3Iq2gUmc5xX/view?usp=sharing" 
+            }
+          ]
+        },
+        {
+          "section_id": "s4",
+          "title": "4- برنامه زمانبندی",
+          "attachments": [
+            { 
+              "title": "برنامه زمانبندی اجرا", 
+              "description": "فایل برنامه زمانبندی", 
+              "file_name": "https://drive.google.com/file/d/1IPfNTJ-2VKkQKYh8AvuuMvNKUFvUIDju/view?usp=sharing" 
+             }
+          ]
+        },
+        {
+          "section_id": "s5",
+          "title": "5- تمامی اسناد و برآورد مناقصه",
+          "attachments": [
+            { 
+              "title": "مجموعه اسناد و برآورد مناقصه", 
+              "description": "دانلود تمامی اسناد", 
+              "file_name": "https://drive.google.com/file/d/1PrI13a_UZweHKiMaZwOWOu3ot52UgJpa/view?usp=sharing" 
+             }
+          ]
+        }
+      ]
+    },
+    {
+      "page_id": "supervision",
+      "title": "نظارت",
+      "subtitle": "مدیریت گزارش‌های کارگاهی و نظارت عالیه",
+      "icon": "construction-site",
+      "order": 2,
+      "sections": [
+        {
+          "section_id": "sup1",
+          "title": "1- گزارش‌های کارگاهی",
+          "attachments": [{ "title": "گزارش‌های ماهانه", "file_name": "assets/Workshop_Reports.rar" }]
+        }
+      ]
+    }
+  ]
+};
